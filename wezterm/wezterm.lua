589ee4c186b271cb8bf6ea75dc157a1dacfcec84
clavesi <34937562+clavesi@users.noreply.github.com>
2025-03-22 11:41:57 -0500

    add basic wezterm config file

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font = wezterm.font_with_fallback{ 'CaskaydiaCove Nerd Font Mono', 'JetBrains Mono' }
+config.font_size = 12.0
+
+-- config.window_decorations = "RESIZE"
+-- config.window_background_opacity = 0.9
+config.use_fancy_tab_bar = false 
+config.tab_bar_at_bottom = true
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" or wezterm.target_triple == "aarch64-pc-windows-msvc" then
+    config.default_prog = { 'pwsh' }
+else
+    config.default_prog = { 'zsh' }
+end
+
+-- and finally, return the configuration to wezterm
+return config
