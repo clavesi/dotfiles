ef662bd4f7bce7e000e1f97cc19b93cc4217103e
clavesi <34937562+clavesi@users.noreply.github.com>
2026-06-11 15:35:08 -0500

    use lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,8 @@
 -- ==========================
--- Theme
+-- lazy.nvim
 -- ==========================
-vim.pack.add{{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" }}
-require("catppuccin").setup({
-    flavour = "mocha",
-    integrations = {
-    	gitsigns = true,
-	    treesitter = true,
-    }
-})
-vim.cmd.colorscheme "catppuccin-nvim"
+require("config.lazy")
+
 
 -- ==========================
 -- OPTIONS
